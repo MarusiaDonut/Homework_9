102aef1ff16aa485ae3755e472a2578bd0db2586
MYudina <MYudina@PC_MSK-20470>
2023-08-08 10:19:37 +0300

    Add project files.

---
+++ Homework_9/ImageDownloader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_9
+{
+    internal class ImageDownloader
+    {
+        public delegate void ImageDownload();
+
+        public event ImageDownload ImageStarted;
+        public event ImageDownload ImageCompleted;
+
+        public bool Download()
+        {
+            string remoteUri = "https://oir.mobi/uploads/posts/2021-06/1623942416_31-oir_mobi-p-neveroyatno-krasivie-peizazhi-priroda-kras-33.jpg";
+            string fileName = "bigimage.jpg";
+
+
+            var myWebClient = new WebClient();
+            ImageStarted();
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+            var task = myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            ImageCompleted();
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+            return task.IsCompleted;
+        }
+    }
+
+    class Handler_Started
+    {
+        public void Message()
+        {
+            Console.WriteLine("Скачивание файла началось");
+        }
+    }
+
+    class Handler_Completed
+    {
+        public void Message()
+        {
+            Console.WriteLine("Скачивание файла закончилось");
+        }
+    }
+
+}
---
+++ Homework_9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Homework_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageDownloader imageDownloader = new ImageDownloader();
+            Handler_Started started = new Handler_Started();
+            Handler_Completed completed = new Handler_Completed();
+
+            imageDownloader.ImageStarted += started.Message;
+            imageDownloader.ImageCompleted += completed.Message;
+
+            var isCompleted = imageDownloader.Download();
+            Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+            var text = Console.ReadLine();
+            if (text == "A")
+            {
+                Environment.Exit(0);
+            }
+            else if (isCompleted == true)
+            {
+                Console.WriteLine("Картинка загружена");
+            }
+            else if (isCompleted == false)
+            {
+                Console.WriteLine("Картинка не загружена");
+            }
+
+        }
+    }
+}
